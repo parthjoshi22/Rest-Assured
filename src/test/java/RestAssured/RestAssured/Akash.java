7aa19283620fc86fc6a65fb13fd24d13c076d852
parthjoshi22 <joshi.parth19@gmail.com>
2024-02-03 16:36:41 +0530

    fourth

--- src/test/java/RestAssured/RestAssured/Akash.java
+++ src/test/java/RestAssured/RestAssured/Akash.java
@@ -4,7 +4,7 @@ public class Akash {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println(" Hi hello");
+System.out.println(" Hi hello Hi");
 	}
 
 }

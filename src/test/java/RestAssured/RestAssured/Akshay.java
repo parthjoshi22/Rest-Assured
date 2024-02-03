1f61ed9603dea99aedd2feb47151253cc499f7b6
parthjoshi22 <joshi.parth19@gmail.com>
2024-02-03 16:21:06 +0530

    second commit

---
+++ src/test/java/RestAssured/RestAssured/Akshay.java
@@ -0,0 +1,11 @@
+package RestAssured.RestAssured;
+
+public class Akshay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" Hi Man");
+	}
+
+}
